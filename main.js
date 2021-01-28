603d6de02660299baa4aa39e5819656f86a1b1b6
BD9jackhammer <60497289+BD9jackhammer@users.noreply.github.com>
2021-01-28 02:58:00 -0600

    Update main.js

--- main.js
+++ main.js
@@ -14,8 +14,8 @@ client.on('message', message =>{
     const args = message.content.slice(prefix.length).split(/ +/);
     const command = args.shift().toLowerCase();
 
-    if(command === 'youtube'){
-        message.channel.send('youtube.com/c/BD9jackhammer/');
+    if(command === 'Test'){
+        message.channel.send('Test Message');
     }
 })
 
